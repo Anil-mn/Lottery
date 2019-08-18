1a6d54246fca8453e2a9274569119ab11b0ade34
ANIL MN <anilmundaya@gmail.com>
2019-08-18 12:30:30 +0530

    script and profile change
    table layout changed in profile page
    function changed into arrow function

--- admin.php
+++ admin.php
@@ -54,38 +54,7 @@
                  { 
                  echo "<tr><td>".$row['id']."</td><td>".$row['username']."</td><td>".$row['number']."</td><td>".$row['password']."</td></tr>"; 
                   }?>
-                    <!-- <tbody>
-                      <tr>
-                        <td>Jacob</td>
-                        <td>53275531</td>
-                        <td>12 May 2017</td>
-                        <td><label class="badge badge-danger">Pending</label></td>
-                      </tr>
-                      <tr>
-                        <td>Messsy</td>
-                        <td>53275532</td>
-                        <td>15 May 2017</td>
-                        <td><label class="badge badge-warning">In progress</label></td>
-                      </tr>
-                      <tr>
-                        <td>John</td>
-                        <td>53275533</td>
-                        <td>14 May 2017</td>
-                        <td><label class="badge badge-info">Fixed</label></td>
-                      </tr>
-                      <tr>
-                        <td>Peter</td>
-                        <td>53275534</td>
-                        <td>16 May 2017</td>
-                        <td><label class="badge badge-success">Completed</label></td>
-                      </tr>
-                      <tr>
-                        <td>Dave</td>
-                        <td>53275535</td>
-                        <td>20 May 2017</td>
-                        <td><label class="badge badge-warning">In progress</label></td>
-                      </tr>
-                    </tbody> -->
+                   
                   </table>
                 </div>
               </div>
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var test,test1,Rup,all
-
-
+var start=100000
+var end=100010
+var options=''
 
 
 function one(x){
@@ -10,12 +11,11 @@ function load(){
      document.getElementById('demo').style.visibility='hidden';
      document.getElementById('bi').style.visibility='hidden';
      document.getElementById('btn').style.visibility='hidden';
-     //document.getElementById('disp').style.visibility='hidden';
+     
     
 
 }
-
-function login()
+login=()=>
 {
     var name = document.getElementById('name').value;
     var pas=document.getElementById('pas').value;
@@ -28,11 +28,13 @@ function login()
     }
    
 }
-function price(){
+
+price=()=>{
  
     var Rupees= document.getElementById('ticket').value;
    
     if(Rupees=='Monsoon Bumper'){
+       
         Rup=200
         price='5 crore'}
      
@@ -57,20 +59,21 @@ function price(){
                         price='6 croro'}
        
         test = one("Rupees");  
-        test.innerHTML ="<h3> cost of a ticket is  "+Rup+"rs </h3>";
+        test.innerHTML ="<h3> Cost of a ticket is  "+Rup+"rs </h3>";
         test1 = one("price");  
-        test1.innerHTML ="<h3>  winning Price of a ticket is  "+price+"rs </h3>";
+        test1.innerHTML ="<h3>  Winning Price of a ticket is  "+price+"rs </h3>";
         document.getElementById('demo').style.visibility='Visible';
         document.getElementById('bi').style.visibility='Visible';
         document.getElementById('btn').style.visibility='hidden';
         document.getElementById('ne').style.visibility='hidden';
 
 }
-function vai(){
+vai=()=>{
     var ltnum= document.getElementById('num').value;
     if(ltnum=='kc-112346'){
-        test = one("vai2");  
-        price='ticket is already booked try another one'
+        test = one("vai");  
+        test.className='text-danger'
+        price='Ticket is already booked try another one'
         test.innerHTML ="<h3>  "+price+" </h3>";
         one('btn').style.visibility='hidden';
         one('ne').style.visibility='hidden';
@@ -78,14 +81,15 @@ function vai(){
     else{
         
         test = one("vai");  
-        price='selected ticket is available '
+        test.className='text-success'
+        price='Selected ticket is available ' 
         test.innerHTML ="<h3>  "+price+" </h3>";
         one('btn').style.visibility='visible'
         one('ne').style.visibility='hidden';
     }
 all=ltnum
 }
-function bu(){
+bu=()=>{
     one('ne').style.visibility='visible'
     test = one("disp");  
        test.innerHTML ="<br><h3> you bought ticket number"+"  "+"  "+all+"  For rs   "+Rup+"  </h3>";
